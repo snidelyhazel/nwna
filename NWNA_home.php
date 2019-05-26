bc4390ede972ca70a7f77022defd04181e7183b7
snidelyhazel <30618085+snidelyhazel@users.noreply.github.com>
2019-05-25 23:23:39 -0700

    [feat] Responsive navigation
    Incl:
    + introduction of collapsed menu icon for small screens
    + prototype of responsive navbar

--- NWNA_home.php
+++ NWNA_home.php
@@ -2,10 +2,20 @@
 <html>
 <head>
   <link rel="stylesheet" type="text/css" href="NWNA_style.css">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
   <header>
     <nav>
+      <div class="topnav" id="NWNA-nav">
+        <a href="#home" class="active">Home</a>
+        <a href="#about">About</a>
+        <a href="#newsletters">Newsletters</a>
+        <a href="#resources">Resources</a>
+        <a href="javascript:void(0);" class="icon" onclick="toggleNav()">
+          <i class="material-icons">menu</i>
+        </a>
+      </div>
 
 <!--
 About
@@ -41,14 +51,30 @@ Resources
 
     </nav>
   </header>
-  <img src="/logo.svg" class="nav-logo" alt="North Westdale Neighborhood Association"/>
+  <img src="logo.svg" class="nav-logo" alt="North Westdale Neighborhood Association"/>
   <p>Hello World!</p>
+
+
+  <script>
+    function toggleNav()
+    {
+      var item = document.getElementById("NWNA-nav");
+      if (item.className === "topnav")
+      {
+        item.className += " responsive";
+      }
+      else
+      {
+        item.className = "topnav";
+      }
+    }
+  </script>
 
   <footer>
     <address>
       North Westdale Neighborhood Association
-      P.O. Box 642522
-      Los Angeles, CA 90064
+      P.O.&nbsp;Box&nbsp;642522
+      Los Angeles,&nbsp;CA&nbsp;90064
     </address>
   </footer>
 </body>
