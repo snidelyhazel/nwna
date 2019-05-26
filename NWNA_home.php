0c790606867a0d0c2d6e3a3e26ba836d654fdf90
snidelyhazel <30618085+snidelyhazel@users.noreply.github.com>
2019-05-25 18:45:42 -0700

    [feat] First commit: start of homepage + CSS stylesheet + logo file
    Incl:
    + homepage with sitemap in comments
    + CSS styling for site
    + logo as SVG

---
+++ NWNA_home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="NWNA_style.css">
+</head>
+<body>
+  <header>
+    <nav>
+
+<!--
+About
+  Join
+  Connect
+Newsletters
+  Current
+  Recent
+  Archives
+    By year
+Meetings
+  Agendas
+  Minutes
+Events
+  Upcoming
+  Past
+Resources
+  NWNA
+    Map
+    Issues
+      Airport
+        Media
+      Development
+        Presentations
+  MVCC
+
+  LA
+    Police
+  Partners
+
+
+-->
+
+    </nav>
+  </header>
+  <img src="/logo.svg" class="nav-logo" alt="North Westdale Neighborhood Association"/>
+  <p>Hello World!</p>
+
+  <footer>
+    <address>
+      North Westdale Neighborhood Association
+      P.O. Box 642522
+      Los Angeles, CA 90064
+    </address>
+  </footer>
+</body>
+</html>
